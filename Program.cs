6704aed96f189463a545d880f4657132b7dbc828
[Denisova Olga] <[denisovazz77@gmail.com]>
2024-02-28 11:45:12 +0300

    add PrintArray

--- Program.cs
+++ Program.cs
@@ -7,3 +7,19 @@
     string[] workArray = enterSymbols.Split(separators, StringSplitOptions.RemoveEmptyEntries);
     return workArray;
 }
+
+string PrintArray(string[] workArray)
+{
+    string stringArray = "[";
+    for (int i = 0; i < workArray.Length; i++)
+    {
+        if (i == workArray.Length - 1)
+        {
+            stringArray += $"\"{workArray[i]}\"";
+            break;
+        }
+        stringArray += ($"\"{workArray[i]}\", ");
+    }
+    stringArray += "]";
+    return stringArray;
+}
